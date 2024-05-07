c10e35780fa1f001f6612d764fe7637375cd0163
0x0redd <101396622+0x0redd@users.noreply.github.com>
2024-05-07 14:12:10 +0100

    fixed boocks in index

--- bibioproject/settings.py
+++ bibioproject/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
          'ENGINE': 'django.db.backends.mysql',
          'NAME': 'biblio',
          'USER': 'root',
-         'PASSWORD': 'admin',
+         'PASSWORD': '',
          'HOST': 'localhost',
          'PORT': '3306',
      }
