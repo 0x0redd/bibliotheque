80af2f99547de76218083f64f1738cc0954902df
0x0redd <101396622+0x0redd@users.noreply.github.com>
2024-05-20 18:52:02 +0100

    deployment fix

--- bibioproject/settings.py
+++ bibioproject/settings.py
@@ -49,8 +49,12 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 ROOT_URLCONF = 'BiblioAPP.urls'
 
